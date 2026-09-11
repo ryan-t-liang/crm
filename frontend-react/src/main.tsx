120a6bb3eeca84361f46691028052276a28e55c0
Ryan <ryan@RyandeMacBook-Pro.local>
2026-09-11 10:38:59 +0800

    feat(crm): apply Kivisense Semi theme

--- frontend-react/src/main.tsx
+++ frontend-react/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
+import "@semi-bot/semi-theme-kivicrm/semi.min.css"
 import { App } from "@/app"
 import "@/index.css"
 
